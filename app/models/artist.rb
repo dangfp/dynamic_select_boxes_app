b253363c493ecd18eee2dfffc6a3270739fa8b5c
dangfp <dangfeipeng@gmail.com>
2013-07-27 08:34:46 +0800

    使用HTML5

---
+++ app/models/artist.rb
@@ -0,0 +1,7 @@
+class Artist < ActiveRecord::Base
+  belongs_to :genre
+ 
+  has_many :songs
+
+  attr_accessible :genre_id, :name, :genre
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicSelectBoxes::Application.config.secret_token = 'c94ba65b0500353398ab533a2013a099e7efe636b2900d2848f556757d110756772ba599d7f5a0d8c76dd3d4492cfd38880b44a654cd3cc80fd4f775141fb0e4'
